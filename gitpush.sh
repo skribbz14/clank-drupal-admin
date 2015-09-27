631ee807692ecd017f841e9762ad1f0fe833ccb0
skribbz14 <kyle@looseygooseyart.com>
2015-09-26 20:15:04 -0400

    Making updates to node_form

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "bug: detect.js was labeling firefox as microsoft-edge"
+git commit -m "Making updates to node_form"
 git push
 cd ../clank_banvas/sites/all/themes/app
 bower update clank-drupal-admin
