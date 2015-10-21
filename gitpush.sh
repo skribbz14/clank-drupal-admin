8d5829012ba9e6e31a8fd3f6f2046f212f874faa
skribbz14 <kyle@looseygooseyart.com>
2015-10-21 18:27:45 -0400

    Working on field collections within node form

--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "Working on the WYSIWYG"
+git commit -m "Working on field collections within node form"
 git push
 cd ../clank/sites/all/themes/app
 bower update clank-drupal-admin
